df13b7786449069acfd1414d9ecd73f8388d64a6
airklizz <remi.calizzano@gmail.com>
2019-10-16 15:13:48 +0200

    micro change

--- data/create_data.py
+++ data/create_data.py
@@ -5,8 +5,10 @@ addr = input("email address : ")
 mdp = input("password : ")
 folder_path = input("folder_path (ex : database/) : ")
 file_name = input("file_name (ex : my_emails) : ")
+begin_mail = input("mail to begin from : ")
+nb_mail = input("number of mail to copy (recommend < 300) : ")
 
-data.create_csv_mail(folder_path+file_name+"_no_translated.csv", addr, mdp) # Create mail database in a csv file of 100 mails in the mailbox
+data.create_csv_mail(folder_path+file_name+"_no_translated.csv", addr, mdp, mails_from_copy=int(begin_mail), mails_to_copy=int(nb_mail)) # Create mail database in a csv file of 100 mails in the mailbox
 
 df = data.get_pandas_from_csv(folder_path+file_name+"_no_translated.csv") 
 df = data.get_txt_from_pandas(df)
